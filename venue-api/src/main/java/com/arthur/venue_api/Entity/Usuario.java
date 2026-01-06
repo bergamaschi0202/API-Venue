f4fd9e7f66aa488887db4b3fbe92561425d9d0fb
Arthur Bergamaschi Souza Sarria <bergamaschiarthur6@gmail.com>
2026-01-06 00:48:23 -0300

    finalizando a classe Usuario

--- venue-api/src/main/java/com/arthur/venue_api/Entity/Usuario.java
+++ venue-api/src/main/java/com/arthur/venue_api/Entity/Usuario.java
@@ -2,6 +2,9 @@ package com.arthur.venue_api.Entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "TBL_USUARIO")
@@ -10,19 +13,54 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "nome")
+    @Column(name = "nome", nullable = false, length = 100)
     private String nome;
 
-    @Column(name = "username")
+    @Column(name = "username", unique = true, nullable = false, length = 50)
     private String username;
 
-    @Column(name = "telefone")
+    @Column(name = "telefone", nullable = false, length = 20)
     private String telefone;
 
-    @Column(name = "email")
+    @Column(name = "email", unique = true, nullable = false, length = 100)
     private String email;
+
+    @Column(name = "senha", nullable = false, length = 255)
+    private String senha;
+
+    @CreationTimestamp
+    @Column(name = "data_cadastro", nullable = false, updatable = false)
+    private LocalDateTime dataCadastro;
+
+    @Column(name = "foto", length = 255)
+    private String foto;
+
+    // Endereço
+    @Column(name = "cep", length = 10)
+    private String cep;
+
+    @Column(name = "logradouro", length = 100)
+    private String logradouro;
+
+    @Column(name = "numero", length = 10)
+    private String numero;
+
+    @Column(name = "complemento", length = 50)
+    private String complemento;
+
+    @Column(name = "bairro", length = 50)
+    private String bairro;
+
+    @Column(name = "cidade", length = 50)
+    private String cidade;
+
+    @Column(name = "estado", length = 2)
+    private String estado;
+
+
+    public Usuario() {
+    }
 
 }
